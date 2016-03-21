cf9908d5718cae6f7136caaefcb7e2f6aa9c499e
Rob Halff <rob.halff@gmail.com>
2016-03-21 02:01:03 +0100

    remove usage of `data` use input.<port> instead

--- nodes/post/node.js
+++ nodes/post/node.js
@@ -1,11 +1,11 @@
 on.input.in = function() {
-  input.db.post(data, function(err, response) {
+  input.db.post(input.in, function(err, response) {
     if(err) {
       output({error: err});
     } else {
       // get full updated document.
       input.db.get(response.id).then(function(doc) {
-        output({out: data});
+        output({out: input.in});
       });
     }
   });
--- nodes/put/node.js
+++ nodes/put/node.js
@@ -1,11 +1,11 @@
 on.input.in = function() {
-  input.db.put(data, function(err, response) {
+  input.db.put(input.in, function(err, response) {
     if(err) {
       output({error: err});
     } else {
       // get full updated document.
       input.db.get(response.id).then(function(doc) {
-        output({out: data});
+        output({out: input.in});
       });
     }
   });
